3f83740190913041eda4ed4079d4df6c5d7e94e2
sejalapeno <110482301+sejalapeno@users.noreply.github.com>
2024-08-27 23:51:47 +0530

    for prod

--- utils/open-ai.ts
+++ utils/open-ai.ts
@@ -1,7 +1,8 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey:
+    "skproj-hmX4_Uin6RavNoYv0jl4GUaFJH4a09dmx4Yc8QVkZeRErqhEYi72i4Gv8IT3BlbkFJlv3pdWF605qkaJEJ3atvgUcmckFsFBMd1IxEHupLTZdq3FMjzEBB2YmyEA",
 });
 
 export async function openAI(description: string) {
